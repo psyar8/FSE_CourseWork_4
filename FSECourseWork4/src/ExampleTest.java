d79b59a8bf7e5f80090f428e2f683a086bf9e04e
Aidan Reed <AidanReed@MacBook-Pro-2.local>
2018-04-24 12:36:02 +0100

    Added ExampleTest JUnit

---
+++ FSECourseWork4/src/ExampleTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	/*
+	 * Author:
+	 * Co-Author: 
+	 * Test ID: 
+	 * Date Tested:
+	 * Test Result: 
+	 */
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
